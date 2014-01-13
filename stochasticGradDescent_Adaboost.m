944911e321e7ffb69e79f38d65d227c608de5b59
sun <pyxuri111@163.com>
2014-01-13 17:40:26 +0800

    version 1

--- stochasticGradDescent_Adaboost.m
+++ stochasticGradDescent_Adaboost.m
@@ -1,36 +1,37 @@
 function stochasticGradDescent_Adaboost
 Y = load('20data.dat');% 225084
-p = randperm(length(Y));
-Y(:,1) = Y(p,1);
-Y(:,2) = Y(p,2);
-Y(:,3) = Y(p,3);
-numTrans = length(Y);
-
+% p = randperm(length(Y));
+% Y(:,1) = Y(p,1);
+% Y(:,2) = Y(p,2);
+% Y(:,3) = Y(p,3);
+num = length(Y);
 first=1;
-testSize=floor(numTrans/5);
+testSize=floor(num/5);
 last = first+testSize-1;
 
 testY = Y(first:last,:);%45016
 trainY = [Y(1:(first-1),:);Y((last+1):end,:)]; % 180068
-weight = 
+
+numTrain = length(trainY);
+
+weightMatrix = ones(numTrain,1)*(1/numTrain);
+%weight
+%errRate
 
 trainSet = sparse(trainY(:,1),trainY(:,2),trainY(:,3));
 testSet = sparse(testY(:,1),testY(:,2),testY(:,3));
-
-k=20;
 %% Training Phase --
 
-[ P,Q ] =model1(full(trainSet),k,testSet);
-
-function [ P,Q ] = model1( L,k,testSet)
+L = full(trainSet);
+k=20;
 %save configs2;
 trainSet = sparse(L);
-nusers = size(L,1);
-nitems = size(L,2);
+nusers = size(L,1);%1429
+nitems = size(L,2);%3191
 
-mu = sum(L(:)) / nnz(L);
-P = rand(nusers,k)/100-0.005;
-Q = rand(nitems,k)/100-0.005;
+mu = sum(L(:))/nnz(L);
+P = rand(nusers,k)/10;
+Q = rand(nitems,k)/10;
 
 [row,col,rating] = find(trainSet);
 [rowT,colT,ratingT] = find(testSet);
@@ -42,14 +43,22 @@ numPasses = 1000;
 Bu = zeros(nusers,1);
 Bi = zeros(nitems,1);
 
-%preTestRmse=100;
 for pass=1:numPasses,
     tempLength = length(row);
     prm = randperm(tempLength);
+    
     for i=1:tempLength
+        a =  prm(1)
+        user = row(prm(1))
+        item = col(prm(1))
+        
         user = row(prm(i));
         item = col(prm(i));
         err = rating(prm(i)) - mu - P(user,:)*Q(item,:)' -Bu(user) -Bi(item);
+        
+        if(err < 0.5 && err > -0.5)
+            fprintf('err %d: \t %e\n', prm(i),err);
+        end
         P(user,:) = P(user,:) + alpha*(err*Q(item,:) - lambda*P(user,:));
         Q(item,:) = Q(item,:) + alpha*(err*P(user,:) - lambda*Q(item,:));
         Bu(user) = Bu(user) + alpha*(err-lambda*Bu(user));
